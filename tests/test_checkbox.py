3e307ee7c27183ec873d4033a6a2695d19c90963
Pravin Alhat <pravin.alhat7@gmail.com>
2025-07-30 14:05:03 +0530

    added new code

---
+++ tests/test_checkbox.py
@@ -0,0 +1,26 @@
+from base.SeleniumDriver import SeleniumDriver
+from page.checkbox import CheckBox
+import pytest
+import unittest
+from utility.variables import Variables
+
+@pytest.mark.usefixtures("OneTimeSetup")
+class PracticePaPgeTest(unittest.TestCase):
+
+    @pytest.fixture(autouse=True)
+    def object_setup(self, OneTimeSetup):
+        self.cb = CheckBox(self.driver)
+
+    def test_selectradiobutton(self):
+        self.cb.select_check_box('bmw')
+        self.cb.select_check_box('benz')
+        self.cb.select_check_box('honda')
+        
+
+
+
+
+
+
+
+#def test_navigate_practice_page(self):
